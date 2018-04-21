949f025c73cbf0019eb2a2bfc0ba0d2832c4df4a
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-21 11:03:32 +1000

    Logging and argument parsing

--- hi_capsule.py
+++ hi_capsule.py
@@ -18,8 +18,72 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 
+import argparse
+import pathlib
+import logging
+import sys
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    # Inputs
+    # TODO: need to work on generalising structure and naming of database
+    parser.add_argument('--database_dir', required=True, type=pathlib.Path,
+            help='Directory containing locus database')
+    parser.add_argument('--query_fp', required=True, type=pathlib.Path,
+            help='Input FASTA query')
+
+    # Options
+    parser.add_argument('--debug', action='store_const', dest='log_level', const=logging.DEBUG,
+            default=logging.WARNING, help='Print debug messages')
+    parser.add_argument('--log_fp', type=pathlib.Path,
+            help='Record logging messages to file')
+
+    args = parser.parse_args()
+
+    # Glob for database files
+    args.database_fps = list(args.database_dir.glob('*fasta'))
+    return args
+
+
 def main():
-    pass
+    args = get_arguments()
+    initialise_logging(args.log_level, args.log_fp)
+    check_arguments(args)
+
+
+def initialise_logging(log_level, log_file):
+    log_handles = list()
+    log_handles.append(logging.StreamHandler())
+    # Ensure that parent dir of log file exists, otherwise raise error during check_arguments
+    if log_file and log_file.exists():
+        log_handles.append(logging.FileHandler(log_file, mode='w'))
+
+    log_message_format = '%(asctime)s %(levelname)s: %(message)s'
+    log_formatter= logging.Formatter(fmt=log_message_format, datefmt='%d/%m/%Y %H:%M:%S')
+    logger = logging.getLogger()
+    for log_handle in log_handles:
+        log_handle.setFormatter(log_formatter)
+        logger.addHandler(log_handle)
+
+    logger.setLevel(log_level)
+
+
+def check_arguments(args):
+    if args.log_fp:
+        check_filepath_exists(args.log_fp.parent, 'Directory %s for log filepath does not exist')
+    check_filepath_exists(args.database_dir, 'Database directory %s does not exist')
+    check_filepath_exists(args.query_fp, 'Input query %s does not exist')
+    if not args.database_fps:
+        msg = 'Could not find any database files (.fasta extension) in %s.'
+        logging.error(msg, args.database_dir)
+        sys.exit(1)
+
+
+def check_filepath_exists(filepath, message_format):
+    if not filepath.exists():
+        logging.error(message_format, filepath)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
