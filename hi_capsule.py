057ebab3e8ad12bd002441f0af23a31ebbc402d1
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-21 11:28:01 +1000

    BLAST alignment for query

--- hi_capsule.py
+++ hi_capsule.py
@@ -19,9 +19,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import argparse
+import collections
 import pathlib
 import logging
+import subprocess
 import sys
+import tempfile
+
+
+BLAST_FORMAT = ['qseqid', 'sseqid', 'qlen', 'slen', 'qstart', 'qend', 'sstart', 'send',
+                'length', 'evalue', 'bitscore', 'pident', 'nident', 'mismatch', 'gaps']
+BlastResults = collections.namedtuple('BlastResults', BLAST_FORMAT)
 
 
 def get_arguments():
@@ -47,9 +55,22 @@ def get_arguments():
 
 
 def main():
+    # Initialise
     args = get_arguments()
     initialise_logging(args.log_level, args.log_fp)
     check_arguments(args)
+
+    # Run
+    with tempfile.TemporaryDirectory() as dh:
+        # Verbose for clarity
+        blast_database_fps = list()
+        for db_fp in args.database_fps:
+            blast_database_fps.append(create_blast_database(db_fp, dh))
+
+        blast_results = list()
+        for blast_db_fp in blast_database_fps:
+            blast_stdout = blast_query(args.query_fp, blast_db_fp)
+            blast_results.append(parse_blast_stdout(blast_stdout))
 
 
 def initialise_logging(log_level, log_file):
@@ -84,6 +105,36 @@ def check_filepath_exists(filepath, message_format):
     if not filepath.exists():
         logging.error(message_format, filepath)
         sys.exit(1)
+
+
+def execute_command(command):
+    logging.debug(command)
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True,
+                            encoding='utf-8')
+    if result.returncode != 0:
+        logging.critical('Failed to run command: %s', result.args)
+        logging.critical('stdout: %s', result.stdout)
+        logging.critical('stderr: %s', result.stderr)
+        sys.exit(1)
+    return result
+
+
+def create_blast_database(input_fp, output_dir):
+    output_fp = pathlib.Path(output_dir, input_fp.name)
+    command = 'makeblastdb -in %s -out %s -dbtype nucl' % (input_fp, output_fp)
+    execute_command(command)
+    return output_fp
+
+
+def blast_query(query_fp, blast_db_fp):
+    command = 'blastn -task blastn -db %s -query %s -outfmt "6 %s"'
+    result = execute_command(command % (blast_db_fp, query_fp, ' '.join(BLAST_FORMAT)))
+    return result.stdout
+
+
+def parse_blast_stdout(blast_results):
+    line_token_gen = (line.split() for line in blast_results.split('\n'))
+    return [BlastResults(*lts) for lts in line_token_gen if lts]
 
 
 if __name__ == '__main__':
