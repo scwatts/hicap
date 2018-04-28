0ce63e190665ddaf2fb3f4d0fa780b47a55bb743
scwatts <s.watts2@student.unimelb.edu.au>
2018-04-29 09:21:30 +1000

    Simplify help text

--- hicap/arguments.py
+++ hicap/arguments.py
@@ -8,34 +8,51 @@ from . import utility
 
 
 def get_args():
-    parser = argparse.ArgumentParser()
+    # Parsers
+    parser_parent = argparse.ArgumentParser()
+    parser_files = parser_parent.add_argument_group('File input and output')
+    parser_params = parser_parent.add_argument_group('Search parameters')
+    parser_other = parser_parent.add_argument_group('Other')
+
     # Inputs
     # TODO: need to work on generalising structure and naming of database
     database_dir = pathlib.Path(__file__).parent / 'database'
-    parser.add_argument('--database_dir', required=False, type=pathlib.Path, default=database_dir,
-            help='Directory containing locus database. [default: %s]' % database_dir)
-    parser.add_argument('--query_fp', required=True, type=pathlib.Path,
+    parser_files.add_argument('--query_fp', required=True, type=pathlib.Path,
             help='Input FASTA query')
-    parser.add_argument('--output_dir', required=True, type=pathlib.Path,
+    parser_files.add_argument('--output_dir', required=True, type=pathlib.Path,
             help='Output directory')
+    parser_files.add_argument('--database_dir', required=False, type=pathlib.Path, default=database_dir,
+            help='Directory containing locus database. [default: %s]' % database_dir)
 
-    # Options
-    parser.add_argument('--gene_coverage', default=0.80, type=float,
+    # Parameters
+    parser_params.add_argument('--gene_coverage', default=0.80, type=float,
             help='Minimum percentage coverage to consider a single gene complete. [default: 0.80]')
-    parser.add_argument('--gene_identity', default=0.75, type=float,
+    parser_params.add_argument('--gene_identity', default=0.75, type=float,
             help='Minimum percentage identity to consider a single gene complete. [default: 0.75]')
-    parser.add_argument('--broken_gene_length', default=60, type=int,
+    parser_params.add_argument('--broken_gene_length', default=60, type=int,
             help='Minimum length to consider a broken gene. [default: 60]')
-    parser.add_argument('--broken_gene_identity', default=0.90, type=float,
+    parser_params.add_argument('--broken_gene_identity', default=0.90, type=float,
             help='Minimum percentage identity to consider a broken gene. [default: 0.90]')
-    parser.add_argument('--debug', action='store_const', dest='log_level', const=logging.DEBUG,
-            default=logging.INFO, help='Print debug messages')
-    parser.add_argument('--log_fp', type=pathlib.Path,
-            help='Record logging messages to file')
 
-    args = parser.parse_args()
+    # Other
+    parser_other.add_argument('--debug', action='store_const', dest='log_level', const=logging.DEBUG,
+            default=logging.INFO, help='Print debug messages')
+    parser_other.add_argument('--log_fp', type=pathlib.Path,
+            help='Record logging messages to file')
+    parser_other.add_argument('--help_all', help='Display extended help')
+
+    # Change behaviour of help display
+    quick_help_args = ('--query_fp', '--output_dir', '--help_all')
+    if '--help_all' in sys.argv[1:]:
+        parser_parent.print_help()
+        sys.exit(0)
+    else:
+        for arg in parser_parent._actions:
+            if not any(qarg in arg.option_strings for qarg in quick_help_args):
+                arg.help = argparse.SUPPRESS
 
     # Glob for database files
+    args = parser_parent.parse_args()
     args.database_fps = list(args.database_dir.glob('*fasta'))
     return args
 
