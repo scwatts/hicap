2cbbf127a8024319f890edb9a9da4577453f7640
scwatts <s.watts2@student.unimelb.edu.au>
2018-09-20 16:50:33 +1000

    Improve graphical output

--- hicap/report.py
+++ hicap/report.py
@@ -1,5 +1,9 @@
+import copy
 import logging
 import pathlib
+import re
+import tempfile
+import xml.etree.ElementTree as ET
 
 
 import Bio.Alphabet
@@ -17,6 +21,7 @@ from . import utility
 
 
 SEQ_PADDING = 1000
+
 COLOURS_COMPLETE = {'one': '#7bcebe',
                     'two': '#f9958b',
                     'three': '#ffe589',
@@ -25,6 +30,14 @@ COLOURS_BROKEN= {'one': '#8fa8a3',
                   'two': '#d3a7a2',
                   'three': '#d8ceab',
                  }
+
+LABEL_SIZE = 12
+TRACK_LABEL_SIZE = 18
+
+HPOINTS_RE = re.compile(r'^([0-9.]+)[^,]+, ([0-9.]+).+$')
+VPOINTS_RE = re.compile(r'^.+ ([0-9.]+)$')
+PATH_RE = re.compile(r'^M ([0-9.]+).+?L ([0-9.]+).+Z$')
+LABEL_RE = re.compile(r'^ matrix\(([-10.]+).+? ([0-9.]+), ([0-9.]+)\)$')
 
 
 class SummaryData:
@@ -49,12 +62,13 @@ def create_report(region_groups, fasta_fp, prefix, output_dir):
     with output_gbk_fp.open('w') as fh:
         Bio.SeqIO.write(genbank_data, fh, 'genbank')
 
-    # Graphic
-    # TODO: clean up graphic. force addition of contig names to short tracks
-    # TODO: legend
-    output_pdf_fp = pathlib.Path(output_dir, '%s.pdf' % prefix)
+    # Create and then patch graphic
+    # TODO: legend?
+    output_svg_fp = pathlib.Path(output_dir, '%s.svg' % prefix)
     graphic_data = create_graphic(genbank_data, prefix)
-    graphic_data.write(str(output_pdf_fp), 'PDF')
+    svg_data = patch_graphic(graphic_data)
+    with output_svg_fp.open('w') as fh:
+        fh.write(svg_data)
 
 
 def create_summary(region_groups):
@@ -173,15 +187,96 @@ def create_graphic(records, prefix):
                 gene_colour = COLOURS_BROKEN[region]
             else:
                 gene_colour = COLOURS_COMPLETE[region]
+            strand = 'start' if feature.strand == 1 else 'end'
             track_features.add_feature(feature, sigil='BIGARROW', label=True, name=gene_name,
-                                       border=reportlab.lib.colors.black, label_position='start',
-                                       label_angle=0, color=gene_colour)
+                                       border=reportlab.lib.colors.black, label_position=strand,
+                                       label_angle=0, label_size=LABEL_SIZE, color=gene_colour)
 
         # TODO: check that height scaling is appropriate
-        height = len(records) * 100
+        # TODO: add contig boundaries symbols
+        height = len(records) * 150
         end = max(len(record) for record in records)
-        graphic.draw(format='linear', pagesize=(1800, height), fragments=1, start=0, end=end)
-        return graphic
+        graphic.draw(format='linear', pagesize=(1800, height), fragments=1, track_size=0.30, start=0, end=end)
+    return graphic
+
+
+def patch_graphic(graphic_data):
+    svg_data = get_svg_data(graphic_data)
+    svg_tree = ET.fromstring(svg_data)
+    visual_parent = svg_tree.find('.//{http://www.w3.org/2000/svg}g[@transform=""]')
+
+    # Get track bound sizes
+    track_style = 'stroke: rgb(96%,96%,96%); stroke-linecap: butt; stroke-width: 1; fill: rgb(96%,96%,96%);'
+    track_backgrounds = svg_tree.findall('.//*[@style="%s"]' % track_style)
+    track_hbounds = {HPOINTS_RE.match(tb.get('points')).groups() for tb in track_backgrounds}
+
+    # Collect mid point lines
+    paths = svg_tree.findall('.//{http://www.w3.org/2000/svg}g[@transform=""]/{http://www.w3.org/2000/svg}path')
+    line_elements = list()
+    for path in paths:
+        path_hbounds = PATH_RE.match(path.get('d')).groups()
+        if path_hbounds in track_hbounds:
+            # Remove mid lines and store for later insert
+            line_elements.append(path)
+            visual_parent.remove(path)
+        elif path_hbounds[0] not in {p for hbounds in track_hbounds for p in hbounds}:
+            # Remove x-axis ticks
+            visual_parent.remove(path)
+
+    # Insert track midpoint lines
+    for line_element in line_elements:
+        visual_parent.insert(len(track_backgrounds), line_element)
+
+    # Fix reversed, mirrored labels. Pad other labels
+    transform_template = ' matrix(1.000000, 0.000000, -0.000000, 1.000000, %s, %s)'
+    texts = svg_tree.findall('.//{http://www.w3.org/2000/svg}g[@transform=""]/{http://www.w3.org/2000/svg}g')
+    for text in texts:
+        a, x, y = LABEL_RE.match(text.get('transform')).groups()
+        if float(a) == -1:
+            y_adjusted = float(y) - LABEL_SIZE
+            transform = transform_template % (x, y_adjusted)
+            text.set('transform', transform)
+        else:
+            y_adjusted = float(y) + 3
+            transform = transform_template % (x, y_adjusted)
+            text.set('transform', transform)
+
+    # Remove original track name label
+    name_style = 'font-family: Helvetica; font-size: 8px; fill: rgb(60%,60%,60%);'
+    names = svg_tree.findall('.//*[@style="%s"]..' % name_style)
+    for name in names:
+        visual_parent.remove(name)
+
+    # Add contig names as track labels
+    contig_names = [track.name for track in graphic_data.tracks.values()]
+    track_vbounds = [VPOINTS_RE.match(tb.get('points')).group(1) for tb in track_backgrounds]
+
+    for insert_index, element in enumerate(visual_parent):
+        if element.tag == '{http://www.w3.org/2000/svg}g':
+            break
+
+    text_format = 'font-family: Helvetica; font-size: %spx; fill: rgb(0%%,0%%,0%%);' % TRACK_LABEL_SIZE
+    text_transform = 'translate(0,0) scale(1,-1)'
+    text_attribs = {'style': text_format, 'transform': text_transform, 'x': '0', 'y': '0'}
+    text_element = ET.Element('{http://www.w3.org/2000/svg}text', attrib=text_attribs)
+    for contig_name, track_vbound, track_hbounds in zip(contig_names, track_vbounds, track_hbounds):
+        x = float(track_hbounds[0])
+        y = float(track_vbound) + TRACK_LABEL_SIZE
+        name_group = ET.Element('{http://www.w3.org/2000/svg}g', attrib={'transform': transform_template % (x, y)})
+        name_text = copy.deepcopy(text_element)
+        name_text.text = contig_name
+        name_group.append(name_text)
+        visual_parent.insert(insert_index, name_group)
+
+    return ET.tostring(svg_tree, encoding='unicode')
+
+
+def get_svg_data(graphic_data):
+    # Writing to string fails under Python3, must write to disk
+    with tempfile.TemporaryFile('w+') as fh:
+        graphic_data.write(fh, 'SVG')
+        fh.seek(0)
+        return fh.read()
 
 
 def get_qualifier(qualifier):
